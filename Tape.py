eee47e187605c31204692bb68d48ed2d6b143558
Stin0banin0 <stin0banin0@outlook.com>
2022-04-06 14:10:07 +0200

    Added planned functions for Tape.py

--- Tape.py
+++ Tape.py
@@ -1,1 +1,18 @@
 #Tape.py is for software engineers what normal tape is for engineers
+
+def getLibraryKeyPosition(item,library):
+    print("This will find the position of a key in a library")
+
+def getLibraryValuePosition(key,library):
+    print("This will find the position of a value in a library")
+
+def getLibraryPositionKey(position,library):
+    print("This will find the key on a position in a library")
+
+def getLibraryPositionValue(position,library):
+    print("This will find the value on a position in a library")
+
+def addUpLibraries(lib1,lib2):
+    for i in range(len(lib2)):
+        key=getLibraryKeyPosition(1,lib2)
+    return lib1
