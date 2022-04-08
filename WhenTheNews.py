0285d27e9a6ae8c7546921da9761d3e6fa89aae1
Stin0banin0 <stin0banin0@outlook.com>
2022-04-08 10:12:08 +0200

    Added things

--- WhenTheNews.py
+++ WhenTheNews.py
@@ -4,10 +4,14 @@ import random
 print("**Welcome to the belated 12:00 news with When The**")
 
 message="Today's main item: "
-subject=capitalise(random.choice(genLib.randSubj))
+subject=genLib.capitalise(random.choice(genLib.randSubj))
 message=message+subject[0]+". "
 
 print(message)
 
 subject=random.choice(genLib.randSubj)
-message=subject[0]+" "+subject[1]+" on the loose. What that means for "+random.choice(randSubj)+" is as of yet unclear, but "+randSubj+
+other=[random.choice(genLib.randSubj),random.choice(genLib.randSubj),random.choice(genLib.randSubj)]
+message="[1] [2] on the loose. What that means for [4] is as of yet unclear, but ".format("yo mama",subject[0],subject[1],subject[2],other[0][0],other[0][1],other[0][2],other[1][0],other[1][1],other[1][2])
+message=subject[0]+" "+subject[1]+" on the loose. What that means for "+random.choice(genLib.randSubj)+" is as of yet unclear, but "+genLib.randSubj+"")
+
+print("Something")
