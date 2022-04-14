dcabfbc89f8b6b4588584110d5d9387afe9296fa
Stin0banin0 <stin0banin0@outlook.com>
2022-04-14 20:17:15 +0200

    First ideas for the DM bot

---
+++ Autonomous Ideas/DM bot.py
@@ -0,0 +1,22 @@
+"""
+The idea of this li'l thing is that it's gonna use data from a bunch of people to hopefully be able to create fun DnD stories. 
+I don't know if it's gonna work, but you never know, and if you don't hope, you're never gonna know. 
+
+Now, what I'm currently thinking of is making some different things. 
+First thing is some scenarios. I might just do very short games about small, short scenarios with friends to see their reactions to the situations.
+
+Example (- is DM,| is player):
+-You're in a prison cell. WDYD?
+|I look around  #Here lookAround is an action
+-The room is small, about 3x4m. There is a small barred window. All walls are made of stone bricks, and one, opposite the window, has a wooden, lined with iron, door in it.
+    In the room there also is a wooden bed and toilet in the form of a hole in the ground. 
+|I try the window bars to see if they're by any chance loose
+-They're firmly in place
+|I knock on the wooden door
+-At first, nothing happens, but suddenly you start hearing footsteps coming down the hall in your direction. A small window in the door is opened and you see a guard's face through
+    it
+|I tell the guard that I'm sick and do my best impression of a sick person
+-The guard enters the room
+|I hit the guard in his face as soon as they come around the door, and use the moment of ruckus to escape and lock him in the cell
+Etc. etc.
+"""
